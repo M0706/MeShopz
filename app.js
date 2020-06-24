f936bec3366164d3a46b77d3ad95adc1c33c2424
Manav Garg <garg.6@iitj.ac.in>
2020-06-24 13:06:59 +0530

    After adding Mysql database using sequelize package

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -36,6 +38,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 
 
 
@@ -56,8 +63,12 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+
+  }).then(cart=>{
     app.listen(3000);
-  })
+  }
+  )
   .catch(err => {
     console.log(err);
   });

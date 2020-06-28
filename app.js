ca3637c58a8a2f594d708acfc3a5a02b1cf87fef
Manav Garg <garg.6@iitj.ac.in>
2020-06-28 16:52:01 +0530

    Added full mongoose functionality with dummy user

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5ef844d92822b2101e0adf3a')
+  User.findById('5ef84b5505c2b811b122483e')
     .then(user => {
       //console.log("user")
       req.user = user;
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Order = require('../models/order');
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -52,14 +52,17 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        }).catch(err => console.log(err));
+    .populate('cart.items.productId')
+    .execPopulate()  //Populate does not return a promise so this returns it
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
@@ -67,11 +70,11 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId)
     .then(product => {
       return req.user.addToCart(product);
-      res.redirect('/cart')
     })
     .then(result => {
       console.log(result);
-    })
+      res.redirect('/cart');
+    });
   // let fetchedCart;
   // let newQuantity = 1;
   // req.user
@@ -107,7 +110,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)
+  req.user.removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -115,24 +118,43 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      //_doc gives only data removing all metadata 
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
+      console.log('Orders === ',orders)
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
-        orders: orders
+        orders: orders.reverse() //Latest order on top
       });
     })
     .catch(err => console.log(err));
 };
+
+

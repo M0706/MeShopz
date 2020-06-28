77e75068791ad1097f65c03ffa9ae6d154a57043
Manav Garg <garg.6@iitj.ac.in>
2020-06-28 12:34:51 +0530

    Added basic mongoose functionality

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose')
+
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const mongoConnect = require('./util/database').mongoConnect;
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,21 +20,27 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5ef4e5c99aed97ff9647b84f')
-    .then(user => {
-      //console.log("user")
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log("Error occured while retrieving the user"));
-});
+// app.use((req, res, next) => {
+//   User.findById('5ef4e5c99aed97ff9647b84f')
+//     .then(user => {
+//       //console.log("user")
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log("Error occured while retrieving the user"));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+
+
+mongoose.connect('mongodb+srv://Manav:jtHknBGHjakFB1Hu@cluster0-mgtvm.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result=>{
+  console.log("Connected")
+  app.listen(3000)
+}).catch(err=>{
+  console.log("Error while connecting")
+})
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,16 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title,
-    price,
-    description,
-    imageUrl,
-    null,
-    req.user._id
-  );
+  const product = new Product({
+    title:title,
+    price:price,
+    description:description,
+    imageUrl:imageUrl,
+  });
   product
-    .save()
+    .save()  //save is provided by mongoose
     .then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -62,16 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
-    .then(result => {
+  Product.findById(prodId).then(product=>{
+    product.title=updatedTitle;
+    product.price=updatedPrice
+    product.description=updatedDesc
+    product.imageUrl=updatedImageUrl
+    return product.save()
+  })
+  .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
@@ -79,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,

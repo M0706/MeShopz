2e2ddb6b1aa5d80a01d72bffc3f31f41fc88076f
Manav Garg <garg.6@iitj.ac.in>
2020-06-28 12:56:29 +0530

    Added a basic dummy user functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
-//const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,15 +20,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5ef4e5c99aed97ff9647b84f')
-//     .then(user => {
-//       //console.log("user")
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log("Error occured while retrieving the user"));
-// });
+app.use((req, res, next) => {
+  User.findById('5ef844d92822b2101e0adf3a')
+    .then(user => {
+      //console.log("user")
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log("Error occured while retrieving the user"));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,6 +40,19 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://Manav:jtHknBGHjakFB1Hu@cluster0-mgtvm.mongodb.net/shop?retryWrites=true&w=majority')
 .then(result=>{
   console.log("Connected")
+  User.findOne().then(user=>{
+    if(!user){
+      const user=new User({
+        name:'Manav',
+        email:"Manavgargpkl@gmail.com",
+        cart:{
+          items:[]
+        }
+      });
+      user.save()
+    }
+  })
+  
   app.listen(3000)
 }).catch(err=>{
   console.log("Error while connecting")
